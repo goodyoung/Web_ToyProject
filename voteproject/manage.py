4c95501450be910ccee93c1ba8d3139f28e27d9b
kanggeowon <kangjiwon5961@gmail.com>
2023-02-17 19:53:09 +0900

    hihi

--- voteproject/manage.py
+++ voteproject/manage.py
@@ -2,8 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
-
+# runserver.default_port = "7070"
 def main():
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -16,7 +17,6 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == "__main__":
     main()
