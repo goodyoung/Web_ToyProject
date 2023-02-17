82ba05464f21c34db1340ee77afc87a384c7b32b
goodyoung <goodyoung.goh@gmail.com>
2023-02-17 19:49:20 +0900

    db delete

--- voteproject/main/models.py
+++ voteproject/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+
 class UserInfo(models.Model):
     id = models.CharField(max_length=30, primary_key=True, blank=False, unique=True)  # unique 는 안했다
     name = models.CharField(max_length=30, blank=False, null = False)
--- voteproject/vote/views.py
+++ voteproject/vote/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-
+from main.models import Registration_title
 def vote(request):
+    print(Registration_title.objects.all())
     return render(request, 'vote/vote_select.html')
