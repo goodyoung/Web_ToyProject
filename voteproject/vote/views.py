002e5c5ab1acdc536a6b8945819250cad25e023b
goodyoung <goodyoung.goh@gmail.com>
2023-02-17 19:04:51 +0900

    base html 수정

--- voteproject/vote/views.py
+++ voteproject/vote/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def vote(request):
-    return HttpResponse("안녕하세요 vote에 오신것을 환영합니다.")
+    return render(request, 'vote/vote_select.html')
